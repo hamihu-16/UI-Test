845d972a8a64a1d4e1e0780fa658833d55873dea
hamihu-16 <100505731+hamihu-16@users.noreply.github.com>
2023-09-19 16:22:25 +0700

    header and popup

--- Assets/Scripts/LanguageSettingsUIHandler.cs
+++ Assets/Scripts/LanguageSettingsUIHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,12 +10,11 @@ using UnityEngine.UI;
 public class LanguageSettingsUIHandler : MonoBehaviour
 {
     [SerializeField] private TMP_Text languageSettingButton;
-
     [SerializeField] private GameObject languagePopUpWindow;
-
     [SerializeField] private List<GameObject> languageButtonList;
-
     [SerializeField] private string currentLanguage = "English";
+
+    public static event Action<string> OnLanguageChanged;
 
     // Start is called before the first frame update
     void Start()
@@ -22,23 +23,42 @@ public class LanguageSettingsUIHandler : MonoBehaviour
         {
             string languageText = button.GetComponentInChildren<TMP_Text>().text;
             Button languageButton = button.GetComponent<Button>();
-            languageButton.onClick.AddListener(() => ChangeLanguage(languageText));
+            languageButton.onClick.AddListener(() => ChangeLanguage(button.gameObject, languageText));
+
+            if (languageText == currentLanguage)
+            {
+                button.GetComponentInChildren<Image>().enabled = true;
+            }
         }
+
     }
 
     public void OpenLanguagePopUpWindow() 
     {
         languagePopUpWindow.SetActive(true);
+        foreach (GameObject button in languageButtonList)
+        {
+            string languageText = button.GetComponentInChildren<TMP_Text>().text;
+            if (languageText == currentLanguage)
+            {
+                button.GetComponentsInChildren<Image>()[1].enabled = true;
+            }
+            else
+            {
+                button.GetComponentsInChildren<Image>()[1].enabled = false;
+            }
+        }
     }
+
     public void CloseLanguagePopUpWindow()
     {
         languagePopUpWindow.SetActive(false);
     }
 
-    private void ChangeLanguage(string language)
+    private void ChangeLanguage(GameObject clickedButton, string languageText)
     {
-        currentLanguage = language;
-        languageSettingButton.text = currentLanguage;
-        languagePopUpWindow.SetActive(false);
+        currentLanguage = languageText;
+        languageSettingButton.text = languageText;
+        CloseLanguagePopUpWindow();
     }
 }
